eeb1ba34e035d5412d100047a733a6a48ed234ff
peiqi.shen <peiqi.shen@dianping.com>
2015-06-16 18:49:40 +0800

    ut

--- src/test/scala/com/github/noahshen/vibranium/strategy/FlowLimitStrategyTest.scala
+++ src/test/scala/com/github/noahshen/vibranium/strategy/FlowLimitStrategyTest.scala
@@ -1,10 +1,23 @@
 package com.github.noahshen.vibranium.strategy
 
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
 
 /**
  * Created by noahshen on 15/6/15.
  */
-class FlowLimitStrategyTest extends FunSuite {
+class FlowLimitStrategyTest extends FunSuite with BeforeAndAfter{
 
+  var flowLimitStrategy: FlowLimitStrategy = _
+
+  before {
+    flowLimitStrategy = new FlowLimitStrategy(5)
+  }
+
+  test("flow limit") {
+    val resOpt = flowLimitStrategy.acquire()
+    assert(resOpt.isDefined)
+    resOpt match {
+      case Some(res) => res.release
+    }
+  }
 }
